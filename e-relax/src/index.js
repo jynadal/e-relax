38a608db0e95427d38dea4c6c2538543db37cde6
jynadal <nadaljeanyves@hotmail.fr>
2020-10-27 00:53:57 +0100

    Add images and beggin slider

--- e-relax/src/index.js
+++ e-relax/src/index.js
@@ -7,16 +7,16 @@ import 'antd/dist/antd.css';
 import '@quasar/extras/ionicons-v4/ionicons-v4.css';
 
 import './assets/css/bootstrap.min.css';
-import './assets/css/core.css';
+//import './assets/css/core.css';
 import './assets/css/style.css';
 import './assets/css/responsive.css';
 import './assets/css/font-awesome.min.css';
 import './assets/css/default.css';
-import './assets/css/material-design-iconic-font.min.css';
-import './assets/plugins/animate.css';
-import './assets/plugins/animated-headlines.css';
-import './assets/plugins/nivo-slider.css';
-import './assets/plugins/owl.carousel.css';
+// import './assets/css/material-design-iconic-font.min.css';
+// import './assets/plugins/animate.css';
+// import './assets/plugins/animated-headlines.css';
+// import './assets/plugins/nivo-slider.css';
+// import './assets/plugins/owl.carousel.css';
 //import './js/jquery.nivo.slider.pack.js"';
 //import './js/main.js"';
 
